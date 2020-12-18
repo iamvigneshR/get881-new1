a4a9aecf1fe0676c39f382ba6e8c0d446bfb62ca
Vignesh <vigneshragu501@gmail.com>
2020-12-18 17:01:00 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -8,6 +8,11 @@ public class Calc {
 		System.out.println("Summation = " +(num1+num2));
 	}
 	
+	public void product(int num1,int num2)
+	{
+		System.out.println("Product = " +(num1*num2));
+	}
+	
 	
 	
 	
